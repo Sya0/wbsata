1cb3e71f8898daf1962aa622253109be29c9fd1c
Sukru Uzun <suk.uzun03@gmail.com>
2025-02-27 00:56:15 +0300

    change naming

--- bench/verilog/mdl_alignp_transmit.v
+++ bench/verilog/mdl_alignp_transmit.v
@@ -2,14 +2,14 @@ module mdl_alignp_transmit (
     input wire clk,
     input wire reset,
     input wire burst_en,
-    input wire [39:0] align_p,
+    input wire [39:0] data_p,
     output reg tx_p,
     output reg tx_n
 );
 
     localparam ALIGNP_BIT = 40;
 
-    reg [ALIGNP_BIT:0] shift_reg;
+    reg [ALIGNP_BIT-1:0] shift_reg;
     reg [5:0] bit_count;
 
     always @(*) begin
@@ -25,21 +25,21 @@ module mdl_alignp_transmit (
     // Sinyal gönderme işlemi
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            shift_reg <= align_p;  // ALIGNP verisini kaydırma register'ına yükle
+            shift_reg <= data_p;  // ALIGNP verisini kaydırma register'ına yükle
             bit_count <= 0;
         end else begin
             if (burst_en) begin
                 // Bir bit gönder ve veriyi kaydır
                 if (bit_count == (ALIGNP_BIT-1)) begin
                     bit_count <= 0;
-                    shift_reg <= align_p;
+                    shift_reg <= data_p;
                 end else begin
                     bit_count <= bit_count + 1;
                     shift_reg <= {shift_reg[38:0], 1'b0}; // Veriyi sola kaydır
                 end
             end else begin
                 // ALIGNP verisi bittiğinde tx_p ve tx_n'i idle seviyede tut
-                shift_reg <= align_p;  // ALIGNP verisini kaydırma register'ına yükle
+                shift_reg <= data_p;  // ALIGNP verisini kaydırma register'ına yükle
             end
         end
     end
